bbe612ce7daa1a06dd52d4d15e81d0b3dc8d4fc2
valkov1989 <123836196+valkov1989@users.noreply.github.com>
2023-01-28 21:46:52 +0200

    Add files via upload

--- usdConvertor.js
+++ usdConvertor.js
@@ -1,7 +1,8 @@
-function convertor(input){
+ function ConvertorUsdToBgn(input){
+  
     let usd = Number(input[0]);
-    let bgn = usd*1.79549;
-
-    console.log(bgn);
-}
-    convertor(["12.5"])
+    let bgn = usd * (1.79549);
+  
+   console.log(bgn);
+ }
+   ConvertorUsdToBgn(["12.5"])
